6d10d47f559deea221acbce5abc9b1b3217bc837
Denise Baumann <denise_baumann@hotmail.com>
2018-12-24 01:28:43 +0100

    production build for gh-pages install gh-pages as dev dependency

---
+++ docs/precache-manifest.8300e7ceb2d3111d44f22d0e0d8a4deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "720c8c78a6b1568a0fe6",
+    "url": "/static/js/main.720c8c78.chunk.js"
+  },
+  {
+    "revision": "577056da6da1bdbc8ea5",
+    "url": "/static/js/1.577056da.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e72b111acdfdc4665ee3b1543166155a",
+    "url": "/static/media/Olympic_rings_with_transparent_rims.e72b111a.svg"
+  },
+  {
+    "revision": "374cfb6661b705367e289d96d5e3e4a0",
+    "url": "/index.html"
+  }
+];
