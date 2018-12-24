886b180cdb7c02d7d6b7484dccc38cdf4227e922
Denise Baumann <denise_baumann@hotmail.com>
2018-12-24 01:46:17 +0100

    Updates

---
+++ precache-manifest.a8ec8e992f0808e77f15952ca4639f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd341394e63bbeb79f42",
+    "url": "/Adilympia/static/js/main.dd341394.chunk.js"
+  },
+  {
+    "revision": "577056da6da1bdbc8ea5",
+    "url": "/Adilympia/static/js/1.577056da.chunk.js"
+  },
+  {
+    "revision": "b9a21fc8e61613618a30",
+    "url": "/Adilympia/static/js/runtime~main.b9a21fc8.js"
+  },
+  {
+    "revision": "e72b111acdfdc4665ee3b1543166155a",
+    "url": "/Adilympia/static/media/Olympic_rings_with_transparent_rims.e72b111a.svg"
+  },
+  {
+    "revision": "93fa942a8af2de290def88107398da53",
+    "url": "/Adilympia/index.html"
+  }
+];
