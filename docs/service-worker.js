36bc12473fa4b7e7f9a90719cf1be2d8c4b7bb7b
Denise Baumann <denise_baumann@hotmail.com>
2018-12-24 02:19:19 +0100

    new build with modified index file

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8300e7ceb2d3111d44f22d0e0d8a4deb.js"
+  "/Adilympia/precache-manifest.a8ec8e992f0808e77f15952ca4639f01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Adilympia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
